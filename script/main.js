27c1f854a76fdaf150c9c3819f5de55ebfcd423d
Fal186 <152485823+Fal186@users.noreply.github.com>
2025-05-03 15:58:01 +0700

    Update main.js

--- script/main.js
+++ script/main.js
@@ -122,7 +122,7 @@ const animationTimeline = () => {
     .to(".idea-3 strong", 0.5, {
         scale: 1.2,
         x: 10,
-        backgroundColor: "rgbrgb(237, 24, 66))",
+        backgroundColor: "rgb(237, 24, 66))",
         color: "#fff",
     })
     .to(".idea-3", 0.7, ideaTextTransLeave, "+=2.5")
