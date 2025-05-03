7df94e96e3bf9ee0ad5fc68c68207f96c2743d55
Fal186 <152485823+Fal186@users.noreply.github.com>
2025-05-03 16:03:17 +0700

    Update main.js

--- script/main.js
+++ script/main.js
@@ -103,7 +103,7 @@ const animationTimeline = () => {
         0.05
     )
     .to(".fake-btn", 0.1, {
-        backgroundColor: "rgb(127, 206, 248)",
+        backgroundColor: "rgb(237, 24, 66))",
     },
     "+=4")
     .to(
